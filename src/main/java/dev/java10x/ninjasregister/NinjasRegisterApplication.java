65cf27f2a5e2fc1cbd15d135e74509f70c1f8118
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-08-28 11:52:47 +0100

    first commit

---
+++ src/main/java/dev/java10x/ninjasregister/NinjasRegisterApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.ninjasregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjasRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjasRegisterApplication.class, args);
+    }
+
+}
