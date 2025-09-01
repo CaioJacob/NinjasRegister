bcacc391b1245f77f5275005be26718d550d6641
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-09-01 20:01:46 +0100

    feat: Put Lombok in the class for abstraction

--- src/main/java/dev/java10x/ninjasregister/Missions/MissionsModel.java
+++ src/main/java/dev/java10x/ninjasregister/Missions/MissionsModel.java
@@ -2,11 +2,18 @@ package dev.java10x.ninjasregister.Missions;
 
 import dev.java10x.ninjasregister.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class MissionsModel {
 
     @Id
@@ -24,45 +31,4 @@ public class MissionsModel {
     private List<NinjaModel> ninjas;
 
 
-    public MissionsModel() {
-    }
-
-    public MissionsModel(Long id, String name, String missionDescription, String missionRank) {
-        this.id = id;
-        this.name = name;
-        this.description = missionDescription;
-        this.rank = missionRank;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
 }
--- src/main/java/dev/java10x/ninjasregister/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/ninjasregister/Ninjas/NinjaModel.java
@@ -2,56 +2,36 @@ package dev.java10x.ninjasregister.Ninjas;
 
 import dev.java10x.ninjasregister.Missions.MissionsModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_register")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class NinjaModel {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private String name;
+    private String name;
 
-        private String email;
+    private String email;
 
-        private int age;
+    private int age;
 
-       // @ManyToOne - The ninja has one only mission!
-       @ManyToOne
-       @JoinColumn(name = "missions_id") // Foreing Key
-       private MissionsModel missions;
+    // @ManyToOne - The ninja has one only mission!
+    @ManyToOne
+    @JoinColumn(name = "missions_id") // Foreing Key
+    private MissionsModel missions;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String name, String email, int age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
+
