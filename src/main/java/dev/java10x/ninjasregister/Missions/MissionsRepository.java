f5a4c6c2a44c68c22f3dd1d6bbb87cf5001272db
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-09-01 23:50:08 +0100

    feat(mission): create MissionRepository extending JpaRepository

---
+++ src/main/java/dev/java10x/ninjasregister/Missions/MissionsRepository.java
@@ -0,0 +1,9 @@
+package dev.java10x.ninjasregister.Missions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionsRepository extends JpaRepository<MissionsModel, Long> {
+
+
+
+}
