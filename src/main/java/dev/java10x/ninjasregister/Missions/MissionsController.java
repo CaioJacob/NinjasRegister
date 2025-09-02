17f7a54e3cd25f4514028d6f8788decd2ff2b511
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-09-02 13:09:29 +0100

    feat: creates controller routes with mock

---
+++ src/main/java/dev/java10x/ninjasregister/Missions/MissionsController.java
@@ -0,0 +1,32 @@
+package dev.java10x.ninjasregister.Missions;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missions")
+public class MissionsController {
+
+    // Show all ninjas (READ)
+    @GetMapping("/list")
+    public String listMissions() {
+        return "List of missions";
+    }
+
+    // Add Mission (CREATE)
+    @PostMapping("/create")
+    public String createMission() {
+        return "Mission created";
+    }
+
+    // Change Mission data (UPDATE)
+    @PutMapping("/alter")
+    public String alterMission() {
+        return "Mission Altered";
+    }
+
+    // Delete ninja (DELETE)
+    @DeleteMapping("/delete")
+    public String deleteMission() {
+        return "Mission Deleted";
+    }
+
+}
