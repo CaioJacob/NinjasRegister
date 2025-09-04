ffaf6da676efb919509dddbbc4287e2654ff0f79
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-09-04 10:38:37 +0100

    chore: includes the DTO version by entity and manually maps the entity x DTO paths in MissionsMapper

--- src/main/java/dev/java10x/ninjasregister/Missions/MissionsDTO.java
+++ src/main/java/dev/java10x/ninjasregister/Missions/MissionsDTO.java
@@ -1,4 +1,24 @@
 package dev.java10x.ninjasregister.Missions;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.java10x.ninjasregister.Ninjas.NinjaModel;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissionsDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String rank;
+    private List<NinjaModel> ninjas;
+
 }
