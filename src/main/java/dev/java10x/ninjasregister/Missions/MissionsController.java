7391a656756152020c1967e6452c10f3b41febfc
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-09-04 20:29:22 +0100

    Refactor(missions): adds final to the injections of my dependencies

--- src/main/java/dev/java10x/ninjasregister/Missions/MissionsController.java
+++ src/main/java/dev/java10x/ninjasregister/Missions/MissionsController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequestMapping("/missions")
 public class MissionsController {
 
-    private MissionsService missionsService;
+    private final MissionsService missionsService;
 
     public MissionsController(MissionsService missionsService) {
         this.missionsService = missionsService;
